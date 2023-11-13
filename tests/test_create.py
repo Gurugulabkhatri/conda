bca771a5e6683ec8840cdf07bd78b9b551ce7aae
Jannis Leidel <jannis@leidel.info>
2023-11-13 14:09:49 +0100

    Fix graphviz.

--- tests/test_create.py
+++ tests/test_create.py
@@ -2900,7 +2900,7 @@ def test_run_script_called():
             "graphviz",
             "--no-deps",
         ) as prefix:
-            assert package_is_installed(prefix, "python.app")
+            assert package_is_installed(prefix, "graphviz")
             assert rs.call_count == 1
 
 
